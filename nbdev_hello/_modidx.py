c64189dfdc2d83e40a073b8d05310d7838c89306
Trivedi <Ashish.Trivedi@adidas.com>
2023-04-16 13:27:04 +0530

    say_hello

--- nbdev_hello/_modidx.py
+++ nbdev_hello/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://Ashish-Adidas.github.io',
                 'git_url': 'https://github.com/Ashish-Adidas/nbdev-hello',
                 'lib_path': 'nbdev_hello'},
-  'syms': {'nbdev_hello.core': {'nbdev_hello.core.foo': ('core.html#foo', 'nbdev_hello/core.py')}}}
+  'syms': { 'nbdev_hello.core': { 'nbdev_hello.core.foo': ('core.html#foo', 'nbdev_hello/core.py'),
+                                  'nbdev_hello.core.say_hello': ('core.html#say_hello', 'nbdev_hello/core.py')}}}
